43c90676ea2f60b8902c3fb8aad8f7e5702550e5
Rubylium <alexisoko0@gmail.com>
2020-07-11 20:20:06 +0200

    Adding banks test command

--- server/testcommands.lua
+++ server/testcommands.lua
@@ -5,3 +5,11 @@ end, false)
 RegisterCommand("removemoney", function(source, args, rawCommand)
     RemoveMoney(source, args[1])
 end, false)
+
+RegisterCommand("addbank", function(source, args, rawCommand)
+    AddBank(source, args[1])
+end, false)
+
+RegisterCommand("removebank", function(source, args, rawCommand)
+    RemoveBank(source, args[1])
+end, false)
