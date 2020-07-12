b886735b10dea8c4194d4f22f1c51df14522b2be
Rubylium <alexisoko0@gmail.com>
2020-07-12 16:25:27 +0200

    Adding inv config

--- config.lua
+++ config.lua
@@ -2,4 +2,10 @@ config = {
     prefix = "rFw:",
     defaultBank = 2000,
     defaultMoney = 500,
+
+    defaultWeightLimit = 50,
+
+    items = {
+        ["test"] = {label = "Item test", weight = 1},
+    }
 }
