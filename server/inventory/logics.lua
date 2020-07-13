3e1f15d4cd8fd78e8a41e1d0e55b6c5f1b011e72
Rubylium <alexisoko0@gmail.com>
2020-07-13 12:06:16 +0200

    Adding AddItemIf + GetInvWeight

--- server/inventory/logics.lua
+++ server/inventory/logics.lua
@@ -49,3 +49,46 @@ function RemoveItem(id, item, count)
         end
     end)
 end
+
+--[[ 
+id = Server ID
+item = Item name, not label
+count = Item count to add
+
+/!\ This **do** check player weight befor giving the item /!\
+]]--
+function AddItemIf(id, item, count)
+    Citizen.CreateThread(function() -- Working in async, maybe that could fix inv issue i got without working in async, need testing i guess
+        if items[item] ~= nil then
+            local iWeight = GetInvWeight(PlayersCache[id].inv)
+            if iWeight + items[item].weight <= config.defaultWeightLimit then
+                if PlayersCache[id].inv[item] == nil then -- Item do not exist in inventory, creating it
+                    PlayersCache[id].inv[item] = {}
+                    PlayersCache[id].inv[item].label = items[item].label
+                    PlayersCache[id].inv[item].count = count
+                else -- Item do exist, adding count
+                    PlayersCache[id].inv[item].count = PlayersCache[id].inv[item].count + count
+                end
+                TriggerClientEvent(config.prefix.."OnGetItem", id, items[item].label, count)
+            else
+                -- Need to do error notification to say, you can't hold the object
+            end
+        else
+            -- Item do not exist, should do some kind of error notification
+            ErrorHandling(id, 1)
+        end
+    end)
+end
+
+
+--[[ 
+inv = Player inventory
+return = weight (int) but could be float
+]]--
+function GetInvWeight(inv)
+    local weight = 0
+    for k,v in pairs(inv) do
+        weight = items[k].weight * v.count
+    end
+    return weight
+end
