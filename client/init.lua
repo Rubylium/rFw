b2cb2fd452e73987761645e57eecc00921d8c9c6
Rubylium <alexisoko0@gmail.com>
2020-07-15 22:24:12 +0200

    Adding PlayerLoaded events to init

--- client/init.lua
+++ client/init.lua
@@ -1,3 +1,4 @@
+local PlayerLoaded = false
 local player = {}
 
 Citizen.CreateThread(function()
@@ -10,12 +11,14 @@ AddEventHandler(config.prefix.."PlayerLoaded", function(playersInfo)
 
     SetEntityCoords(GetPlayerPed(-1), player.pos, 0.0, 0.0, 0.0, 0)
     InitPosLoop()
+    PlayerLoaded = true
 end)
 
 RegisterNetEvent(config.prefix.."OnJobChange")
 AddEventHandler(config.prefix.."OnJobChange", function(job, grade)
     player.job = job
     player.job_grade = grade
+    TriggerEvent("rFw:JobChange", job, grade)
 end)
 
 RegisterNetEvent(config.prefix.."OnInvRefresh")
@@ -23,6 +26,9 @@ AddEventHandler(config.prefix.."OnInvRefresh", function(inv)
     player.inv = inv
 end)
 
+function IsPlayerLoaded()
+    return PlayerLoaded
+end
 
 function GetPlayerJob()
     return player.job, player.job_grade
