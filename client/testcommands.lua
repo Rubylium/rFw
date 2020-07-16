1dd147b67acd62e4a6d514a7deaf253437b85409
Rubylium <alexisoko0@gmail.com>
2020-07-16 11:25:38 +0200

    Update testcommands.lua

--- client/testcommands.lua
+++ client/testcommands.lua
@@ -13,3 +13,7 @@ RegisterCommand("car", function(source, args, rawCommand)
     local veh = CreateVehicle(GetHashKey(args[1]), GetEntityCoords(GetPlayerPed(-1)), GetEntityHeading(GetPlayerPed(-1)), 1, 0)
     TaskWarpPedIntoVehicle(GetPlayerPed(-1), veh, -1)
 end, false)
+
+RegisterCommand("pos", function(source, args, rawCommand)
+    print(GetEntityCoords(GetPlayerPed(-1)))
+end, false)
