b7b510b2c66c0f142cb0fe3bc87254c7a33c4b0c
Rubylium <alexisoko0@gmail.com>
2020-07-28 17:23:48 +0200

    Update players.sql

--- players.sql
+++ players.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 19, 2020 at 07:31 PM
+-- Generation Time: Jul 28, 2020 at 03:23 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 5.6.40
 
@@ -32,6 +32,7 @@ DROP TABLE IF EXISTS `players`;
 CREATE TABLE IF NOT EXISTS `players` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `license` varchar(50) NOT NULL,
+  `skin` text DEFAULT NULL,
   `accounts` text NOT NULL,
   `perm_level` int(11) NOT NULL,
   `job` varchar(255) NOT NULL,
@@ -40,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `players` (
   `pos` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `license` (`license`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -73,15 +74,7 @@ CREATE TABLE IF NOT EXISTS `society` (
   `money` int(11) NOT NULL DEFAULT 0,
   `inventory` text DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `society`
---
-
-INSERT INTO `society` (`id`, `society_name`, `money`, `inventory`) VALUES
-(1, 'test_society', 1000, '{}'),
-(2, 'test_society2', 1500, '{}');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
