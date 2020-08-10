69c73c297063b9a38d853784cea569632e2cc0a0
Rubylium <alexisoko0@gmail.com>
2020-08-10 12:57:05 +0200

    Remove debug print

--- server/inventory/logics.lua
+++ server/inventory/logics.lua
@@ -189,7 +189,6 @@ function DoesItemExistWithArg(id, item, arg)
     if arg == nil then arg = {} end
     for k,v in pairs(PlayersCache[id].inv) do
         if v.item == item then
-            print(json.encode(v.args), json.encode(arg), json.encode(v.args) == json.encode(arg))
             if json.encode(v.args) == json.encode(arg) then
                 return true, v.itemId
             end
