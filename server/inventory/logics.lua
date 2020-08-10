5d1833cadf9c9f062b580fafa5cc56b2fa7ad5bd
Rubylium <alexisoko0@gmail.com>
2020-08-10 11:55:34 +0200

    :warning: Reworked AddItem
    Starting to rework inventory logics. Every item will now have an unique ID, so i will be able to make a args table to pass everything in it. Ammo count, label etc ...
    :warning: THIS DO BREAK THE CURRENT INVENTORY SYSTEM :warning:

--- server/inventory/logics.lua
+++ server/inventory/logics.lua
@@ -8,23 +8,29 @@ count = Item count to add
 
 /!\ This do not check player weight befor giving the item /!\
 ]]--
-function AddItem(id, item, count)
-    Citizen.CreateThread(function() -- Working in async, maybe that could fix inv issue i got without working in async, need testing i guess
-        if items[item] ~= nil then
-            if PlayersCache[id].inv[item] == nil then -- Item do not exist in inventory, creating it
-                PlayersCache[id].inv[item] = {}
-                PlayersCache[id].inv[item].label = items[item].label
-                PlayersCache[id].inv[item].count = count
-            else -- Item do exist, adding count
-                PlayersCache[id].inv[item].count = PlayersCache[id].inv[item].count + count
+function AddItem(id, item, count, args)
+    if items[item] ~= nil then
+        local exist, itemid = DoesItemExistWithArg(id, item, args)
+        if not exist then -- Item do not exist in inventory, creating it
+            itemid = GenerateItemId()
+            PlayersCache[id].inv[itemid] = {}
+            PlayersCache[id].inv[itemid].item = item
+            PlayersCache[id].inv[itemid].label = items[item].label
+            PlayersCache[id].inv[itemid].count = count
+            PlayersCache[id].inv[itemid].itemId = itemid
+            PlayersCache[id].inv[itemid].args = {}
+            if args ~= nil then
+                PlayersCache[id].inv[itemid].args = args
             end
-            TriggerClientEvent(config.prefix.."OnGetItem", id, items[item].label, count)
-            TriggerClientEvent(config.prefix.."OnInvRefresh", id, PlayersCache[id].inv, GetInvWeight(PlayersCache[id].inv))
-        else
-            -- Item do not exist, should do some kind of error notification
-            ErrorHandling(id, 1)
+        else -- Item do exist, adding count
+            PlayersCache[id].inv[itemid].count = PlayersCache[id].inv[itemid].count + count
         end
-    end)
+        TriggerClientEvent(config.prefix.."OnGetItem", id, items[item].label, count)
+        TriggerClientEvent(config.prefix.."OnInvRefresh", id, PlayersCache[id].inv, GetInvWeight(PlayersCache[id].inv))
+    else
+        -- Item do not exist, should do some kind of error notification
+        ErrorHandling(id, 1)
+    end
 end
 
 --[[ 
@@ -33,26 +39,24 @@ item = Item name, not label
 count = Item count to remove
 ]]--
 function RemoveItem(id, item, count)
-   --Citizen.CreateThread(function()
-        if items[item] ~= nil then
-            if PlayersCache[id].inv[item] ~= nil then -- Item do not exist in inventory
-                if PlayersCache[id].inv[item].count - count <= 0 then -- If count < or = 0 after removing item count, then deleting it from player inv
-                    PlayersCache[id].inv[item] = nil
-                else
-                    PlayersCache[id].inv[item].count = PlayersCache[id].inv[item].count - count
-                end
-                TriggerClientEvent(config.prefix.."OnRemoveItem", id, items[item].label, count)
-                TriggerClientEvent(config.prefix.."OnInvRefresh", id, PlayersCache[id].inv, GetInvWeight(PlayersCache[id].inv))
-                return true
+    if items[item] ~= nil then
+        if PlayersCache[id].inv[item] ~= nil then -- Item do not exist in inventory
+            if PlayersCache[id].inv[item].count - count <= 0 then -- If count < or = 0 after removing item count, then deleting it from player inv
+                PlayersCache[id].inv[item] = nil
             else
-                ErrorHandling(id, 2)
-                return false
+                PlayersCache[id].inv[item].count = PlayersCache[id].inv[item].count - count
             end
+            TriggerClientEvent(config.prefix.."OnRemoveItem", id, items[item].label, count)
+            TriggerClientEvent(config.prefix.."OnInvRefresh", id, PlayersCache[id].inv, GetInvWeight(PlayersCache[id].inv))
+            return true
         else
-            ErrorHandling(id, 1)
+            ErrorHandling(id, 2)
             return false
         end
-    --end)
+    else
+        ErrorHandling(id, 1)
+        return false
+    end
 end
 
 --[[ 
@@ -62,7 +66,7 @@ count = Item count to add
 
 /!\ This **do** check player weight befor giving the item /!\
 ]]--
-function AddItemIf(id, item, count)
+function AddItemIf(id, item, count, args)
     --Citizen.CreateThread(function() -- Working in async, maybe that could fix inv issue i got without working in async, need testing i guess
         if items[item] ~= nil then
             local iWeight = GetInvWeight(PlayersCache[id].inv)
@@ -71,8 +75,39 @@ function AddItemIf(id, item, count)
                     PlayersCache[id].inv[item] = {}
                     PlayersCache[id].inv[item].label = items[item].label
                     PlayersCache[id].inv[item].count = count
+                    PlayersCache[id].inv[item].args = {}
+                    PlayersCache[id].inv[item].itemId = GenerateItemId()
+                    if args ~= nil then
+                        PlayersCache[id].inv[item].args = args
+                    end
                 else -- Item do exist, adding count
-                    PlayersCache[id].inv[item].count = PlayersCache[id].inv[item].count + count
+                    if PlayersCache[id].inv[item].args ~= nil then
+                        if json.encode(PlayersCache[id].inv[item].args) ~= json.encode(args) then
+                            PlayersCache[id].inv[item] = {}
+                            PlayersCache[id].inv[item].label = items[item].label
+                            PlayersCache[id].inv[item].count = count
+                            PlayersCache[id].inv[item].args = {}
+                            PlayersCache[id].inv[item].itemId = GenerateItemId()
+                            if args ~= nil then
+                                PlayersCache[id].inv[item].args = args
+                            end
+                        else
+                            PlayersCache[id].inv[item].count = PlayersCache[id].inv[item].count + count
+                        end
+                    else
+                        if json.encode(PlayersCache[id].inv[item].args) ~= json.encode(args) then
+                            PlayersCache[id].inv[item] = {}
+                            PlayersCache[id].inv[item].label = items[item].label
+                            PlayersCache[id].inv[item].count = count
+                            PlayersCache[id].inv[item].args = {}
+                            PlayersCache[id].inv[item].itemId = GenerateItemId()
+                            if args ~= nil then
+                                PlayersCache[id].inv[item].args = args
+                            end
+                        else
+                            PlayersCache[id].inv[item].count = PlayersCache[id].inv[item].count + count
+                        end
+                    end
                 end
                 TriggerClientEvent(config.prefix.."OnGetItem", id, items[item].label, count)
                 TriggerClientEvent(config.prefix.."OnInvRefresh", id, PlayersCache[id].inv, GetInvWeight(PlayersCache[id].inv))
@@ -129,8 +164,39 @@ function ExhangeItem(id, target, item, count, countsee)
                     PlayersCache[target].inv[item] = {}
                     PlayersCache[target].inv[item].label = items[item].label
                     PlayersCache[target].inv[item].count = count
+                    PlayersCache[target].inv[item].args = {}
+                    PlayersCache[id].inv[item].itemId = GenerateItemId()
+                    if PlayersCache[id].inv[item].args ~= nil then
+                        PlayersCache[target].inv[item].args = PlayersCache[id].inv[item].args
+                    end
                 else -- Add to count
-                    PlayersCache[target].inv[item].count = PlayersCache[target].inv[item].count + count
+                    if PlayersCache[target].inv[item].args ~= nil then
+                        if json.encode(PlayersCache[id].inv[item].args) ~= json.encode(PlayersCache[target].inv[item].args) then
+                            PlayersCache[target].inv[item] = {}
+                            PlayersCache[target].inv[item].label = items[item].label
+                            PlayersCache[target].inv[item].count = count
+                            PlayersCache[target].inv[item].args = {}
+                            PlayersCache[id].inv[item].itemId = GenerateItemId()
+                            if args ~= nil then
+                                PlayersCache[target].inv[item].args = args
+                            end
+                        else
+                            PlayersCache[target].inv[item].count = PlayersCache[target].inv[item].count + count
+                        end
+                    else
+                        if json.encode(PlayersCache[id].inv[item].args) ~= json.encode(PlayersCache[target].inv[item].args) then
+                            PlayersCache[target].inv[item] = {}
+                            PlayersCache[target].inv[item].label = items[item].label
+                            PlayersCache[target].inv[item].count = count
+                            PlayersCache[target].inv[item].args = {}
+                            PlayersCache[id].inv[item].itemId = GenerateItemId()
+                            if args ~= nil then
+                                PlayersCache[target].inv[item].args = args
+                            end
+                        else
+                            PlayersCache[target].inv[item].count = PlayersCache[target].inv[item].count + count
+                        end
+                    end
                 end
                 TriggerClientEvent(config.prefix.."OnGetItem", target, items[item].label, count)
                 TriggerClientEvent(config.prefix.."OnInvRefresh", target, PlayersCache[id].inv, tWeight)
@@ -142,6 +208,31 @@ function ExhangeItem(id, target, item, count, countsee)
 end
 
 
+function SetItemArg(id, itemId, arg)
+    for k,v in pairs(PlayersCache[id].inv) do
+        if v.itemId == itemId then
+            v.args = arg
+        end
+    end
+
+    TriggerClientEvent(config.prefix.."OnInvRefresh", id, PlayersCache[id].inv, GetInvWeight(PlayersCache[id].inv))
+end
+
+function GenerateItemId()
+    return ""..tostring(math.random(100001,900009)).."-"..tostring(math.random(100001,900009)).."-"..tostring(math.random(100001,900009))
+end
+
+function DoesItemExistWithArg(id, item, arg)
+    for k,v in pairs(PlayersCache[id].inv) do
+        if v.item == item then
+            if json.encode(v.args) == json.encode(arg) then
+                return true, v.itemId
+            end
+        end
+    end
+    return false
+end
+
 --[[ 
 inv = Player inventory
 return = weight (int) but could be float
@@ -149,7 +240,6 @@ return = weight (int) but could be float
 function GetInvWeight(inv)
     local weight = 0
     for k,v in pairs(inv) do
-        print(v.label, v.count)
         weight = items[k].weight * v.count
     end
     return weight
