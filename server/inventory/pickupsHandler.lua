57d7abd08c20618d5750d4dbd0365f047cc1dfb3
Rubylium <alexisoko0@gmail.com>
2020-08-18 01:46:24 +0200

    Remove debug print

--- server/inventory/pickupsHandler.lua
+++ server/inventory/pickupsHandler.lua
@@ -1,7 +1,6 @@
 local pickups = {}
 
 function DropItem(id, item, itemid, count, coords)
-    print(id, item, itemid, count, coords)
     if RemoveItem(id, item, itemid, count) then
         pickups[#pickups+1] = {item = item, count = count, coords = coords, added = false}
         TriggerClientEvent(config.prefix.."SendAllPickups", -1, pickups)
